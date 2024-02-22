8c81ed87a2e8f9c8c91e9929a5e02cb7b28cfcdb
SuryaTej-N <sxn2261@mavs.uta.edu>
2024-02-21 18:41:19 -0600

    implemented the API fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 
 import React from 'react';
 
-import { useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+
+import { useEffect , useState } from 'react';
 
 // api key for movies : c211aa0a
 
@@ -10,10 +13,13 @@ const API_URL = 'https://www.omdbapi.com/?apikey=c211aa0a';
 
 function App() {
 
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data);
+
+    setMovies(data.Search);
   }
 
   useEffect(() => {
@@ -21,7 +27,39 @@ function App() {
   }, []);
 
   return (
-     <h1>App </h1>
+     <div className="app">
+       <h1>Moviebuzz</h1>
+
+        {/* now lets add a search bar */}
+
+        <div className="search">
+          <input
+            placeholder="Search for movies"
+            value="spiderman"
+            onChange={() => {}}
+          />
+          <img
+            src={SearchIcon}
+            alt="search"
+            onClick={() => {}}
+          />
+        </div>
+
+        {/* movies will be listed here */}
+        
+        { movies?.length >0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+
+     </div>
   );
 }
 
